b25ad309d6b1bd1340dffa758db429e02d7627b1
Peter Donker <peter@bring2mind.net>
2022-07-06 10:48:32 +0200

    Initial commit

---
+++ MyCommand.cs
@@ -0,0 +1,22 @@
+﻿using DotNetNuke.Abstractions.Prompt;
+using DotNetNuke.Prompt;
+
+namespace DnnPromptTest
+{
+  [ConsoleCommand("hello-world", "My Commands", "This does a great job at greeting")]
+  public class MyCommand : ConsoleCommand
+  {
+    public override string LocalResourceFile => "~/App_GlobalResources/Prompt.resx";
+
+    public override IConsoleResultModel Run()
+    {
+      var res = new ConsoleResultModel()
+      {
+        IsError = false,
+        IsHtml = false,
+        Output = $"Hello World and {User.DisplayName}"
+      };
+      return res;
+    }
+  }
+}
